a3a258714ab86829fc8c7ea9c810ffd5647d8baf
Mascris <amineserrar20@gmail.com>
2025-11-23 18:38:44 +0100

    some minor changes today

--- OOP-Porject/contact_book.py
+++ OOP-Porject/contact_book.py
@@ -1,3 +1,12 @@
 class Contact:
-    def __init__(self,name: str,phone_number: int,email=None, address=None) -> None:
+    def __init__(self,name: str, phone_number: str,email=None,address=None) -> None:
+        self.name = name
+        self.phone_number = phone_number
+        self.email = email
+        self.address = address
+    
+    def update_info(self,phone_number=None,email=None,address=None):
+        
 
+    def display_info(self):
+        print(f"M.{self.name},who's living in {self.address}, phone number:{self.phone_number}, email: {self.email}.")
